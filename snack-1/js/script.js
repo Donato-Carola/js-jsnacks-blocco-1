7701a5916f5b1a3e71980e3214011bb90fa744c0
Donato-Carola <don94@live.it>
2023-11-10 11:52:37 +0100

    chiesto gli anni con prompt

---
+++ snack-1/js/script.js
@@ -0,0 +1,10 @@
+
+//chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
+
+
+const firstAge = prompt('inserisci gli anni della prima persona');
+const secondAge = prompt('inserisci gli anni della prima persona');
+
+if (firstAge > secondAge) {
+    
+}
