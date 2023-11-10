208041949783cae4dc5e187898fb23b91617848e
Donato-Carola <don94@live.it>
2023-11-10 11:57:44 +0100

    provando con length

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -2,9 +2,10 @@
 //chiedere all'utente con due prompt gli anni di due persone e comunicare quale delle due è più grande
 
 
-const firstAge = prompt('inserisci gli anni della prima persona');
-const secondAge = prompt('inserisci gli anni della prima persona');
+const firstAge = prompt('inserisci gli anni della prima persona').length;
+const secondAge = prompt('inserisci gli anni della prima persona').length;
 
 if (firstAge > secondAge) {
-    
+
+    console.log('numero più grande è:', firstAge, secondAge)
 }
