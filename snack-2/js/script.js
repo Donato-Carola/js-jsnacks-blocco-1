fce22d00d593e8072b3c299a839892b608193ca9
Donato-Carola <don94@live.it>
2023-11-14 12:19:12 +0100

    stampato i numeri a schermo senza colore

--- snack-2/js/script.js
+++ snack-2/js/script.js
@@ -2,7 +2,11 @@
 
 // ! Scrivo in pagina i primi 6 numeri, scrivendo i numeri pari verdi. BN: numeri dispari gialli
 
+const listElem = document.querySelector('div.list');
 
 for (let i=1; i<=6; i++){
-    
+     const elem=`<h1>${i}</h1>`;
+     listElem.innerHTML += elem;
+
+
 }
