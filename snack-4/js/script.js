2a4287d8de6bb0e3ee69a9df2b7129345c31ca10
Donato-Carola <don94@live.it>
2023-11-15 12:06:46 +0100

    esercizio snack

---
+++ snack-4/js/script.js
@@ -0,0 +1,12 @@
+//! Ciclare per i primi 100 numeri e inserire in un array esclusivamente i numeri divisibili per 7 o per 8.
+const num =[];
+
+
+for (let i=1; i<=100; i++){
+    //console.log(i)
+    if(i % 7 == 0 || i % 8 == 0){
+        num.push(i);   
+        console.log(i);
+    }
+
+}
