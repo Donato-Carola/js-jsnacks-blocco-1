283cb9dc913db46a04f1b49dba234678428b5cf1
Donato-Carola <don94@live.it>
2023-11-16 16:04:28 +0100

    base esercizio 2 , da riguardare

---
+++ snack-8/js/script.js
@@ -0,0 +1,18 @@
+//! Snack B:
+// ? Chiedi un numero di 7 cifre all'utente e calcola la somma di tutte le cifre 
+//? che compongono il numero.
+
+const inputUser=document.querySelector('button')
+
+inputUser.addEventListener("click", 
+function(){
+
+   const numberUser = document.querySelector('input').value;
+   console.log(numberUser);
+
+   /*Scompongo il numero per avere ogni singola cifra */
+   /*quindi avendo tutte le cifre del numero faccio la somma*/
+
+
+})
+
