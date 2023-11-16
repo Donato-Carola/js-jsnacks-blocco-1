ced8368ec1cee9a01bfbea9d9bdb26fcf3632a71
Donato-Carola <don94@live.it>
2023-11-16 15:09:49 +0100

    primo esercizio da riguardare

---
+++ snack-7/js/script.js
@@ -0,0 +1,26 @@
+
+// !Snack A:
+//Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero,
+// se è dispari inseriscilo nell'array.
+
+//? Creo un array vuoto
+
+const array = [];
+
+
+//? Chiedere all'utente i 6 numeri
+for (let i=1; i<=6;i++){
+const numberUser = prompt(`inserisci numero ${i}`);
+console.log(numberUser);
+if(numberUser % 2 === 1){
+    array.push(numberUser);
+    console.log(`numeri dispari in disordine: ${array} `);
+}
+
+ array.sort();
+ console.log(`numeri dispari in ordine: ${array} `);
+
+}
+
+
+
