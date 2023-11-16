33fc98ec5126d16d15ef47df82ce65488f0f784d
Donato-Carola <don94@live.it>
2023-11-16 17:01:07 +0100

    esercizio 10 concluso, penso

---
+++ snack-10/js/script.js
@@ -0,0 +1,18 @@
+//!Snack D (bonus):
+//Inserisci un <h1> al centro della pagina con un numero randomico all'interno e un bottone sotto. 
+//Ad ogni click del bottone aumento il numero nell'h1 di 3 e lo aggiorno in pagina.
+
+const number = document.getElementById('bigNumber')
+
+
+//number.innerHTML=(`${randomNumber}`)
+
+const button = document.querySelector('button');
+
+button.addEventListener("click",
+function(){
+    const randomNumber = (Math.random());
+    console.log(randomNumber) ;
+    const multi = randomNumber * 3;
+    number.innerHTML=(`numero random <br> ${randomNumber} x 3 = ${multi}`);
+})
