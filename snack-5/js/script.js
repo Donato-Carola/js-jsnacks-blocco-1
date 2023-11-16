35a02607d120f3a376e72dc6b5f0c1c8de8c277a
Donato-Carola <don94@live.it>
2023-11-16 12:26:18 +0100

    prima prova sulla lunghezza

---
+++ snack-5/js/script.js
@@ -0,0 +1,13 @@
+
+//!L'utente inserisce due parole in successione, con due prompt.
+//!Il software stampa prima la parola più corta, poi la parola più lunga.
+
+const firstUser = prompt(' utente uno inserisci parola');
+const secondUser = prompt('utente due inserisci parola');
+
+const lengthFirstWord= firstUser.length;
+console.log(lengthFirstWord);
+
+const lengthSecondWord = secondUser.length;
+console.log(lengthSecondWord);
+
